261ffd82f6e8f2f60cd93115d71759efe9d6f02b
Dennis Yu <dennisyuxx@hotmail.com>
2020-01-19 03:40:18 -0500

    update view path

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Search />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Search from '@/components/HomeSearch.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Search
   }
 }
 </script>
---
+++ src/views/Result.vue
@@ -0,0 +1,14 @@
+<template>
+  <Result />
+</template>
+
+<script>
+import Result from "@/components/Result.vue";
+
+export default {
+  name: "result",
+  components: {
+    Result
+  }
+};
+</script>
