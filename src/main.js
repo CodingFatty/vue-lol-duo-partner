f861b978acec6de397d5640b74829b1e19c3a896
Dennis Yu <dennisyuxx@hotmail.com>
2020-01-19 03:36:42 -0500

    add axios & highchart

--- src/main.js
+++ src/main.js
@@ -2,7 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+import Highcharts from 'highcharts';
+import VueHighcharts from 'vue-highcharts';
+import loadDrilldown from 'highcharts/modules/drilldown.js';
+
 Vue.config.productionTip = false
+
+Vue.use(VueAxios, axios);
+
+loadDrilldown(Highcharts);
+Vue.use(VueHighcharts, { Highcharts });
 
 new Vue({
   router,
